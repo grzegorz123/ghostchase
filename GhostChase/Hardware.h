3eadbf7b3400b6195d797349c16cbbf700be7be9
Ricklon <rick.rickanderson@gmail.com>
2013-02-20 00:09:30 -0500

    Added Fritzing tweaked sketch.

---
+++ GhostChase/Hardware.h
@@ -0,0 +1,18 @@
+#ifndef Hardware_h
+#define Hardware_h
+/*
+* Uncomment out the Hardware combination you have
+* Platform: Teensy, Adafruit: TFTst7735, SD
+*/
+
+#define SD_CS    4  // Chip select line for SD card
+#define TFT_CS   0  // Chip select line for TFT display
+#define TFT_DC   8  // Data/command line for TFT
+#define TFT_RST  9  // Reset line for TFT (or connect to +5V)
+
+#define TILT_PINA 16
+#define TILT_PINB 17
+#define TILT_PINC 13
+#define TILT_PIND 15
+
+#endif
